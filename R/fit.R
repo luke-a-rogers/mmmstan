171f9d7d011a7d79e5d58fe661d83d653e068b1f
luke-a-rogers <luke.allister.rogers@gmail.com>
2021-09-23 14:36:02 -0700

    updated to perform rough argument check; updated to output prior arguments in parameters list

--- R/fit.R
+++ R/fit.R
@@ -1,6 +1,6 @@
 #' Fit Markov Movement Model via CmdStanR
 #'
-#' @param data [list()]
+#' @param data [list()] See details
 #' @param chains [integer()] number of chains
 #' @param step_size [numeric()] initial step size
 #' @param iter_warmup [integer()] number of warmup iterations
@@ -8,6 +8,8 @@
 #' @param use_reduce_sum [logical()] use within chain parallel threading
 #' @param threads_per_chain [integer()] number of threads per chain
 #' @param ... additional arguments to pass to \code{$sample()} method
+#'
+#' @details TBD
 #'
 #' @return [mmmfit()]
 #' @export
@@ -23,8 +25,33 @@ mmmfit <- function (data,
 
   # Check arguments ------------------------------------------------------------
 
+  # Check T_study
+  T_released <- data$T_released
+  T_liberty <- data$T_liberty
+  T_study <- data$T_study
+  if (T_study == T_liberty) {
+    if (T_study > T_released) {
+      cat("\nT_study == T_liberty & T_study > T_released\n")
+    } else {
+      stop("T_study must be greater than T_released\n")
+    }
+  } else if (T_study == T_liberty + T_released - 1) {
+    cat("T_study == T_liberty + T_released - 1\n")
+  } else {
+    stop("T_study must equal T_liberty or T_liberty + T_released - 1\n")
+  }
+  # Check harvest prior parameters
+  h_prior_mean <- data$h_prior_mean
+  h_prior_sd <- data$h_prior_sd
+  if (!all((h_prior_sd * h_prior_sd) < (h_prior_mean * (1 - h_prior_mean)))) {
+    stop("all((h_prior_sd^2) < (h_prior_mean*(1-h_prior_mean))) must be true\n")
+  }
+
+
   # Check that max(rowSums(data$z)) <= 6
   # Check that if rw == 1, P > 2
+  # Check dimensions
+  # Check lengths of indices
 
 
   # Fit the model via cmdstanr -------------------------------------------------
@@ -49,12 +76,24 @@ mmmfit <- function (data,
     ...
   )
 
+  # Assemble parameters --------------------------------------------------------
+
+  parameters <- summary_to_tibbles(
+    cmdfit$summary(),
+    data$h_prior_mean,
+    data$h_prior_sd,
+    data$phi_prior_mean,
+    data$phi_prior_sd,
+    data$sigma_prior_mean,
+    data$sigma_prior_sd
+  )
+
   # Return mmmfit object -------------------------------------------------------
 
   structure(list(
     data = data,
     output = cmdfit$output_files(),
     summary = cmdfit$summary(),
-    parameters = summary_to_tibbles(cmdfit$summary())),
+    parameters = parameters),
     class = "mmmfit")
 }
